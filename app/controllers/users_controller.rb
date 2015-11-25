9966dc540160b6d3011389f9f0a899871ecb1209
CaseyKelly <casey.kelly@colorado.edu>
2015-11-25 14:53:53 -0500

    update create action with flash errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :home, notice: 'Account created successfully'
     else
-      redirect_to '/signup'
+      flash[:error] = 'An error occured!'
+      render 'new'
     end
   end
 
