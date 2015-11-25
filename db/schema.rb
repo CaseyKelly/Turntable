8a0eb18d6dc1f46867f28236f6f33fd5a5ec367d
CaseyKelly <casey.kelly@colorado.edu>
2015-11-25 15:36:56 -0500

    add dropbox_session to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125030437) do
+ActiveRecord::Schema.define(version: 20151125203550) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151125030437) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "dropbox_session"
   end
 
 end
